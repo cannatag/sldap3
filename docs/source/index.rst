4ab723ba9542a953b1e25e75168b5fcc6bcd9ea8
cannatag <cannatag@gmail.com>
2015-02-20 22:02:27 +0100

    added sphinx

---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. sldap3 documentation master file, created by
+   sphinx-quickstart on Fri Feb 20 21:34:00 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sldap3's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ sldap3/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY IT!!!# version file for sldap3
+# generated on 2015-02-20 21:08:56.143905
+# on system uname_result(system='Windows', node='GCNBHPW8', release='8', version='6.2.9200', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
+# with Python 3.4.2 - ('v3.4.2:ab2c023a9432', 'Oct  6 2014 22:16:31') - MSC v.1600 64 bit (AMD64)
+#
+__version__ = '0.0.1'
+__author__ = 'Giovanni Cannata'
+__email__ = 'cannatag@gmail.com'
+__url__ = 'https://github.com/cannatag/ldap3'
+__description__ = 'A strictly RFC 4511 conforming LDAP V3 pure Python server.'
+__status__ = 'development - alpha'
+__license__ = 'LGPL v3'
