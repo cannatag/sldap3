ac308ba81120b52e30e6489d63d1b7057cea961a
cannatag <cannatag@gmail.com>
2015-02-21 08:11:01 +0100

    fixed setup

--- sldap3/version.py
+++ sldap3/version.py
@@ -1,12 +1,12 @@
 # THIS FILE IS AUTO-GENERATED. PLEASE DO NOT MODIFY!!!# version file for sldap3
-# generated on 2015-02-21 07:59:07.065785
+# generated on 2015-02-21 08:02:45.675972
 # on system uname_result(system='Windows', node='GCNBHPW8', release='8', version='6.2.9200', machine='AMD64', processor='Intel64 Family 6 Model 58 Stepping 9, GenuineIntel')
 # with Python 3.4.2 - ('v3.4.2:ab2c023a9432', 'Oct  6 2014 22:16:31') - MSC v.1600 64 bit (AMD64)
 #
-__version__ = '0.0.1-alpha'
+__version__ = '0.0.1a1'
 __author__ = 'Giovanni Cannata'
 __email__ = 'cannatag@gmail.com'
 __url__ = 'https://github.com/cannatag/sldap3'
-__description__ = 'A strictly RFC 4511 conforming LDAP V3 pure Python server.'
+__description__ = 'A strictly RFC 4511 conforming LDAP V3 pure Python server. Alpha version - DO NOT USE!!!'
 __status__ = 'development - alpha'
 __license__ = 'LGPL v3'
