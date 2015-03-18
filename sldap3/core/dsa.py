4a2acc1c9dc10f9440d315cc00f783d46a4a9774
cannatag <cannatag@gmail.com>
2015-03-18 22:15:59 +0100

    created operation package

--- sldap3/core/dsa.py
+++ sldap3/core/dsa.py
@@ -24,9 +24,12 @@
 # If not, see <http://www.gnu.org/licenses/>.
 
 import asyncio
+import ssl
 
 from ldap3 import SEQUENCE_TYPES, LDAPControlsError
 from ldap3.core.exceptions import LDAPExceptionError
+from ldap3.strategy.base import BaseStrategy
+from ldap3.protocol.convert import build_controls_list
 from ldap3.operation.abandon import abandon_request_to_dict
 from ldap3.operation.bind import bind_request_to_dict
 from ldap3.operation.add import add_request_to_dict
@@ -50,26 +53,26 @@ def client_connected(reader, writer):
     dsa = asyncio.get_event_loop()._dsa
     user = User()
     task = dsa.loop.create_task(dsa.handle_client(reader, writer, user))
-    print('new connection')
+    print('new connection on server', dsa.name)
     dsa.clients[task] = (reader, writer, user)
 
     def client_done(task_done):
-        print('closing connection')
+        print('closing connection on server', dsa.name, 'for user', user.identity)
         del dsa.clients[task_done]
         writer.close()
 
     task.add_done_callback(client_done)
 
+
 class Dsa(object):
-    def __init__(self, address, port, use_ssl=False):
+    def __init__(self, name, address, port, use_ssl=False):
+        self.name = name
         self.address = address
         self.port = port
         self.use_ssl = use_ssl
         self.clients = dict()
         self.server = None
-        self.loop = asyncio.new_event_loop()
-        self.loop._dsa = self
-        asyncio.set_event_loop(self.loop)
+        self.loop = None
 
     @asyncio.coroutine
     def status(self):
@@ -77,21 +80,30 @@ class Dsa(object):
         trigger = False
         while True:
             if len(self.clients) != last:
-                print('Clients:', len(self.clients))
+                print('Clients on server', self.name + ':', len(self.clients))
                 last = len(self.clients)
             yield from asyncio.sleep(2)
             if self.clients:
                 trigger = True
             if trigger and not self.clients:
                 break
-        print('Closing server')
+        print('Closing server', self.name)
         self.server.close()
-        print('server closed')
+        print('server {} closed'.format(self.name))
 
     def start(self):
-        coro = asyncio.start_server(client_connected, self.address, self.port)
+        self.loop = asyncio.new_event_loop()
+        self.loop._dsa = self
+        asyncio.set_event_loop(self.loop)
+        if self.use_ssl:
+            ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+            ssl_context.options &= ~ssl.OP_NO_SSLv3
+            coro = asyncio.start_server(client_connected, self.address, self.port, ssl=ssl_context)
+        else:
+            print('start_server', self.name)
+            coro = asyncio.start_server(client_connected, self.address, self.port)
         self.server = self.loop.run_until_complete(coro)
-        print('Server started')
+        print('Server {} started'.format(self.name))
         self.loop.create_task(self.status())
 
         try:
@@ -118,7 +130,7 @@ class Dsa(object):
                     data = yield from reader.read(4096)
                     unprocessed += data
                 if len(data) > 0:
-                    length = Dsa.compute_ldap_message_size(unprocessed)
+                    length = BaseStrategy.compute_ldap_message_size(unprocessed)
                     if length == -1:  # too few data to decode message length
                         get_more_data = True
                         continue
@@ -132,21 +144,22 @@ class Dsa(object):
                             receiving = False
                 else:
                     receiving = False
-            print('received {} bytes'.format(len(data)))
+            print('received {} bytes for server {}'.format(len(data), self.name))
             if messages:
                 for request in messages:
                     while len(request) > 0:
                         ldap_req, unprocessed = decoder.decode(request, asn1Spec=LDAPMessage())
                         request = unprocessed
                         self.loop.create_task(self.perform_request(writer, ldap_req, user))
-                    print('processed request')
-        print('exit handle')
+                    print('processed request for server', self.name)
+        print('exit handle for server', self.name)
 
     @asyncio.coroutine
     def perform_request(self, writer, request, user):
-        print('performing request', request)
+        print('performing request', request, 'on server', self.name)
         message_id = int(request.getComponentByName('messageID'))
-        dict_req = self.decode_request(request)
+        dict_req = BaseStrategy.decode_request(request)
+
         if dict_req['type'] == 'bindRequest':
             response = yield from do_bind_operation(self, user, message_id, dict_req)
             response_type = 'bindResponse'
@@ -162,7 +175,7 @@ class Dsa(object):
         ldap_message['messageID'] = MessageID(message_id)
         ldap_message['protocolOp'] = ProtocolOp().setComponentByName(response_type, response)
         controls = None
-        message_controls = Dsa.build_controls_list(controls)
+        message_controls = build_controls_list(controls)
         if message_controls is not None:
             ldap_message['controls'] = message_controls
 
@@ -170,105 +183,3 @@ class Dsa(object):
         encoded_message = encoder.encode(ldap_message)
         writer.write(encoded_message)
         writer.drain()
-
-    @staticmethod
-    def decode_request(ldap_message):
-        message_type = ldap_message.getComponentByName('protocolOp').getName()
-        component = ldap_message['protocolOp'].getComponent()
-        if message_type == 'bindRequest':
-            result = bind_request_to_dict(component)
-        elif message_type == 'unbindRequest':
-            result = dict()
-        elif message_type == 'addRequest':
-            result = add_request_to_dict(component)
-        elif message_type == 'compareRequest':
-            result = compare_request_to_dict(component)
-        elif message_type == 'delRequest':
-            result = delete_request_to_dict(component)
-        elif message_type == 'extendedReq':
-            result = extended_request_to_dict(component)
-        elif message_type == 'modifyRequest':
-            result = modify_request_to_dict(component)
-        elif message_type == 'modDNRequest':
-            result = modify_dn_request_to_dict(component)
-        elif message_type == 'searchRequest':
-            result = search_request_to_dict(component)
-        elif message_type == 'abandonRequest':
-            result = abandon_request_to_dict(component)
-        else:
-            raise Exception('unknown request')
-        result['type'] = message_type
-        return result
-
-    @staticmethod
-    def compute_ldap_message_size(data):
-        """
-        Compute LDAP Message size according to BER definite length rules
-        Returns -1 if too few data to compute message length
-        """
-        if isinstance(data, str):  # fix for Python 2, data is string not bytes
-            data = bytearray(data)  # Python 2 bytearray is equivalent to Python 3 bytes
-
-        ret_value = -1
-        if len(data) > 2:
-            if data[1] <= 127:  # BER definite length - short form. Highest bit of byte 1 is 0, message length is in the last 7 bits - Value can be up to 127 bytes long
-                ret_value = data[1] + 2
-            else:  # BER definite length - long form. Highest bit of byte 1 is 1, last 7 bits counts the number of following octets containing the value length
-                bytes_length = data[1] - 128
-                if len(data) >= bytes_length + 2:
-                    value_length = 0
-                    cont = bytes_length
-                    for byte in data[2: 2 + bytes_length]:
-                        cont -= 1
-                        value_length += byte * (256 ** cont)
-                    ret_value = value_length + 2 + bytes_length
-
-        return ret_value
-
-    @staticmethod
-    def build_controls_list(controls):
-        """
-        controls is a list of tuple
-        each tuple must have 3 elements: the control OID, the criticality, the value
-        criticality must be a boolean
-        """
-        if not controls:
-            return None
-
-        if not isinstance(controls, SEQUENCE_TYPES):
-            raise LDAPControlsError('controls must be a list')
-
-        built_controls = Controls()
-        for idx, control in enumerate(controls):
-            if len(control) == 3 and isinstance(control[1], bool):
-                built_control = Control()
-                built_control['controlType'] = control[0]
-                built_control['criticality'] = control[1]
-                built_control['controlValue'] = control[2]
-                built_controls.setComponentByPosition(idx, built_control)
-            else:
-                raise LDAPControlsError(
-                    'control must be a tuple of 3 elements: controlType, criticality (boolean) and controlValue')
-
-        return built_controls
-
-    @staticmethod
-    def decode_control(control):
-        """
-        decode control, return a 2-element tuple where the first element is the control oid
-        and the second element is a dictionary with description (from Oids), criticality and decoded control value
-        """
-        control_type = str(control['controlType'])
-        criticality = bool(control['criticality'])
-        control_value = bytes(control['controlValue'])
-        if control_type == '1.2.840.113556.1.4.319':  # simple paged search as per RFC2696
-            control_resp, unprocessed = decoder.decode(control_value, asn1Spec=RealSearchControlValue())
-            control_value = dict()
-            control_value['size'] = int(control_resp['size'])
-            control_value['cookie'] = bytes(control_resp['cookie'])
-            if unprocessed:
-                pass
-
-        return control_type, {'description': Oids.get(control_type, ''), 'criticality': criticality,
-                              'value': control_value}
-
--- sldap3/start.py
+++ sldap3/start.py
@@ -1,8 +1,23 @@
-import asyncio
 from core.dsa import Dsa
+from core.instance import Instance
+from multiprocessing import Process
 
-dsa = Dsa('localhost', 389)
-dsa.start()
+instances = []
 
-dsa2 = Dsa('localhost', 1389)
-dsa2.start()
+dsa1 = Instance(Dsa('DSA1', 'localhost', 389, use_ssl=False))
+dsa2 = Instance(Dsa('DSA2', 'localhost', 1389, use_ssl=False))
+
+instances.append(dsa1)
+instances.append(dsa2)
+
+
+for instance in instances:
+    instance.process = Process(target=instance.dsa.start)
+    instance.process.start()
+
+print('started %d instances' % (len(instances)))
+
+for instance in instances:
+    instance.process.join()
+
+print('sldap3 done')
