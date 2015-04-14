4f2a8b326a29c820c36de4cea13ac4b34f8128a5
cannatag <cannatag@gmail.com>
2015-04-14 09:11:52 +0200

    added windows service management

--- sldap3/win-service-sldap3.py
+++ sldap3/win-service-sldap3.py
@@ -2,10 +2,8 @@ import win32serviceutil
 import win32service
 import win32event
 import servicemanager
-import socket
 import logging
 from multiprocessing import Process
-from sys import exit, path
 
 logging.basicConfig(
     filename='c:\\Temp\\sldap3.log',
@@ -15,25 +13,23 @@ logging.basicConfig(
 
 logging.info('start log')
 try:
-    path.append('C:\\Python\\OpenSource\\sldap3')
     from sldap3 import JsonUserBackend, Dsa, Instance
 except ImportError:
     logging.error('PATH: ' + str(path))
-    logging.error('sldap3 package missing')
+    logging.error('sldap3 or ldap3 package missing')
     exit(1)
 
 
 class Sldap3Service (win32serviceutil.ServiceFramework):
     _svc_name_ = 'sldap3'
     _svc_display_name_ = 'sldap3 - LDAP Server'
+    _svc_description_ = 'A striclty RFC 4511 conforming LDAP V3 pure Python server'
 
-    def __init__(self,args):
+    def __init__(self, args):
         logging.info('Initializing class...')
 
         win32serviceutil.ServiceFramework.__init__(self, args)
         self.stop_event = win32event.CreateEvent(None, 0, 0, None)
-        socket.setdefaulttimeout(60)
-        self.stop_requested = False
         self.instances = list()
 
     def SvcStop(self):
@@ -44,13 +40,14 @@ class Sldap3Service (win32serviceutil.ServiceFramework):
             instance.stop()
 
     def SvcDoRun(self):
-        logging.info('Starting service...')
+        logging.info('Running service...')
         servicemanager.LogMsg(
             servicemanager.EVENTLOG_INFORMATION_TYPE,
             servicemanager.PYS_SERVICE_STARTED,
             (self._svc_name_, '')
         )
-        self.main()
+        self.main2()
+        logging.info('Ending service...')
 
     def main(self):
         logging.info('Executing service...')
@@ -70,6 +67,8 @@ class Sldap3Service (win32serviceutil.ServiceFramework):
             for instance in self.instances:
                 instance.process = Process(target=instance.dsa.start)
                 instance.process.start()
+            for instance in self.instances:  # wait for all instances to end
+                instance.process.join()
         elif len(self.instances) == 1:  # use the same thread
             self.instances[0].dsa.start()
 
